ad50d1fd8e8c8cf5d8444f970fcaafc933e0bf45
vikash <vikash@vikash-PC>
2017-06-05 01:01:40 +0530

    Second commit

--- src/allduplacyavoid/Reverse.java
+++ src/allduplacyavoid/Reverse.java
@@ -3,9 +3,11 @@ package allduplacyavoid;
 public class Reverse {
 
 	public static void main(String[] args) {
+		//this is mt second commit
 		String s="Vikash-Kumar";
 		String s2=new StringBuffer(s.substring(0,6)).reverse().toString()+"-"+new StringBuffer(s.substring(7,12)).reverse().toString();
 System.out.println(s2);
+System.out.println("Changes done by me to show the detail");
 	}
 
 }
